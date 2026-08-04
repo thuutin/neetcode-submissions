0c36e6372b672b29dd7a35a2a34cdf049ae116ef
Tran Huu Tin <thuutin@gmail.com>
2026-08-05 05:43:18 +0700

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        f1 = defaultdict(int)
+        for c in s1:
+            f1[c] += 1
+        s = len(s1)
+        f = defaultdict(int)
+        for i in range(len(s2)):
+            f[s2[i]] += 1
+            if i - s >= 0:
+                f[s2[i - s]] -= 1
+                if f[s2[i - s]] == 0:
+                    del f[s2[i - s]]
+            if i + 1 < s:
+                continue
+            
+            if f1 == f:
+                return True
+            
+        return False
+            
+
+
+            
