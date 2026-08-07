dbd787a4adb0facc2484e2c872bc4c1ea0a51823
Tran Huu Tin <thuutin@gmail.com>
2026-08-07 18:51:14 +0700

    Add: implement-prefix-tree - submission-7

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-7.py
@@ -0,0 +1,34 @@
+
+class PrefixTree:
+
+    def __init__(self, char = "$", ternimal = False):
+        self.nodes = {}
+        self.char = char
+        self.ternimal = ternimal
+
+    def insert(self, word: str, i = 0) -> None:
+        c = word[i]
+        if c not in self.nodes:
+            self.nodes[c] = PrefixTree(c)
+        if i < len(word) - 1:
+            self.nodes[c].insert(word, i + 1)
+        else:
+            self.nodes[c].ternimal = True
+
+    def search(self, word: str, i = 0) -> bool:
+        c = word[i]
+        if c not in self.nodes:
+            return False
+        if i < len(word) - 1:
+            return self.nodes[c].search(word, i + 1)
+        else:
+            return self.nodes[c].ternimal
+        
+    def startsWith(self, prefix: str, i = 0) -> bool:
+        c = prefix[i]
+        if c not in self.nodes:
+            return False
+        if i < len(prefix) - 1:
+            return self.nodes[c].startsWith(prefix, i + 1)
+        return True
+        
