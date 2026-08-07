8dfffd93741196b9f26a9497942c15e9166f10b9
Tran Huu Tin <thuutin@gmail.com>
2026-08-07 19:54:55 +0700

    Add: search-for-word-ii - submission-5

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-5.py
@@ -0,0 +1,57 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        #(4 ^ k) * m * n * len(words) 
+        #4^10 * 30k * 12 * 12
+        m = len(board)
+        n = len(board[0])
+        MAX_WORD = 0
+        for word in words:
+            MAX_WORD = max(MAX_WORD, len(word))
+        class Node:
+            def __init__(self):
+                self.children = {}
+        
+        root = Node()
+
+        def search(word):
+            curr = root
+            for c in word:
+                if c not in curr.children:
+                    return False
+                curr = curr.children[c]
+            return True
+
+        ## build trie
+        def backtrack(i, j, path, trie):
+   
+            path.add((i, j))
+            c = board[i][j]
+            if c not in trie.children:
+                trie.children[c] = Node()
+            node = trie.children[c]
+
+            for di, dj in [(0, 1),(0, -1),(1, 0),(-1, 0)]:
+                ni, nj = i + di, j + dj
+                if ni < 0 or ni >= m:
+                    continue
+                if nj < 0 or nj >= n:
+                    continue
+                if (ni, nj) in path:
+                    continue
+                if len(path) >= MAX_WORD:
+                    break
+                c = board[ni][nj]
+                backtrack(ni, nj, path, node)
+            path.remove((i, j))
+            
+
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                path = set()
+                backtrack(i, j, path, root)
+        
+        res = []
+        for word in words:
+            if search(word):
+                res.append(word)
+        return res
