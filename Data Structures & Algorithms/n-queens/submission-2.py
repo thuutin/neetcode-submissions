ff9de57f37d6b4fc7064f0bb29944e692e4895ad
Tran Huu Tin <thuutin@gmail.com>
2026-08-07 18:24:01 +0700

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def valid(queens, i , j):
+            for qi, qj in enumerate(queens):
+                if qj == j:
+                    return False
+                if qi + qj == i + j or qi - qj == i - j:
+                    return False
+            return True
+        res = []
+        def generate(queens):
+            board = []
+            for i in range(n):
+                qj = queens[i]
+                row = ["Q" if j == qj else "." for j in range(n)]
+                row = "".join(row)
+                board.append(row)
+            return board
+
+        def backtrack(i, queens):
+            if i >= n:
+                res.append(generate(queens))
+            for j in range(n):
+                if valid(queens, i, j):
+                    queens.append(j)
+                    backtrack(i + 1, queens)
+                    queens.pop()
+                
+        backtrack(0, [])
+        return res
