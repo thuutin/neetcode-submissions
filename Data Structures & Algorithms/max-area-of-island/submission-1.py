3c851d517872f3bd0befd36c63cc7f67b90f820b
Tran Huu Tin <thuutin@gmail.com>
2026-08-08 04:53:25 +0700

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,46 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        parent = [i for i in range(m * n)]
+        size = [1] * (m * n)
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            root_x = find(x)
+            root_y = find(y)
+            if root_x == root_y:
+                return False
+            parent[root_y] = root_x
+
+            size[root_x] += size[root_y]
+            return True
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    x = i * n + j
+                    for di, dj in [(0, 1),(0, -1),(1, 0),(-1, 0)]:
+                        ii, jj = di + i, dj + j
+                        if ii < 0 or ii >= len(grid):
+                            continue
+                        if jj < 0 or jj >= len(grid[i]):
+                            continue
+                        if grid[ii][jj] == 1:
+                            y = ii * n + jj
+                            union(x, y)
+        #print(parent)
+        maxSize = 0
+        #print(size)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    maxSize = max(maxSize, size[find(i*n+j)])
+
+        return maxSize
+
+
+        
