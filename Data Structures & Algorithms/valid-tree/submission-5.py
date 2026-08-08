3b765202e2c3aa3204f35548ca97157520e4dbc9
Tran Huu Tin <thuutin@gmail.com>
2026-08-08 22:55:28 +0700

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        q = deque([(0, None)])
+        visit = set([0])
+        while q:
+            node, parent = q.popleft()
+            for neibour in adj[node]:
+                if neibour == parent:
+                    continue
+                if neibour in visit:
+                    return False
+                visit.add(neibour)
+                q.append((neibour, node))
+        return len(visit) == n
+ 
