b3be19b8309c61d27016c21ece4ef1449fd439d7
Tran Huu Tin <thuutin@gmail.com>
2026-08-08 18:03:14 +0700

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        q = []
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    heapq.heappush(q,(0, i, j)  )
+        dirs = [(0, -1),(0, 1),(1, 0),(-1, 0)]
+        while q:
+            d, i, j = heapq.heappop(q)
+            for di, dj in dirs:
+                ni, nj = di + i, dj + j
+                if ni < 0 or ni >=m or nj < 0 or nj >= n:
+                    continue
+                if grid[ni][nj] == -1:
+                    continue
+                if grid[ni][nj] <= d + 1:
+                    continue
+                grid[ni][nj] = d + 1
+                heapq.heappush(q, ((d+1, ni, nj)))
+
+        return None
