221f847c2780c2c561ea9cbef6501bbba7656974
Tran Huu Tin <thuutin@gmail.com>
2026-08-10 01:37:37 +0700

    Add: distribute-candies-among-children-ii - submission-4

---
+++ Data Structures & Algorithms/distribute-candies-among-children-ii/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        res = 0
+        for a in range(max(0, n - limit * 2), min(limit, n) + 1):
+            left = n - a
+            if left > 2 * limit:
+                continue
+            if left > limit:
+                minValue = left - limit
+                maxValue = limit
+                res += abs(maxValue - minValue) + 1
+            else:
+                res += left + 1
+        return res
+        #x 
+        #n - a <= limit * 2
+        # 
