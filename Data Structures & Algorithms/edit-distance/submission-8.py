9ef17f3a951853e00c6f53f491487df81f8afb58
Tran Huu Tin <thuutin@gmail.com>
2026-08-11 04:44:06 +0700

    Add: edit-distance - submission-8

---
+++ Data Structures & Algorithms/edit-distance/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        prev = [0] * (len(word2) + 1)
+        dp = [0] * (len(word2) + 1)
+        for i in range(len(word1) + 1):
+
+            for j in range(len(word2) + 1):
+                dp[j] = 0
+                if i == 0:
+                    dp[j] = j
+                elif j == 0:
+                    dp[j] = i
+                else:
+                    match_cost = prev[j - 1]
+                    change_cost = prev[j - 1] + 1
+                    add_cost = dp[j - 1] + 1
+                    delete_cost = prev[j] + 1
+                    if word1[i-1] == word2[j-1]:
+                        dp[j] = min(add_cost, delete_cost, match_cost)
+                    else:
+                        dp[j] = min(add_cost, delete_cost, change_cost)
+            prev, dp = dp, prev
+            
+        return prev[-1]
