45cfabf3d16edc03bf2e49ccd381846638fec524
Tran Huu Tin <thuutin@gmail.com>
2026-08-12 00:46:02 +0700

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l, r = 0, 0
+        step = 0
+        while r < len(nums) - 1:
+            max_reachable = r
+            for i in range(l, r + 1):
+                max_reachable = max(max_reachable, i + nums[i])
+            l, r = r + 1, max_reachable
+            step += 1
+        return step
+                
+        # [2,2,1,1,1,1]
+        # [3,3,2,2,1,0]
+        # [0,1,1,2,3,4]
