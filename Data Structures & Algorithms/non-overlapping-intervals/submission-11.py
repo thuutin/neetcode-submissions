5ca886cbbaa2e28b8a733f36706bf538eea912b6
Tran Huu Tin <thuutin@gmail.com>
2026-08-12 21:54:12 +0700

    Add: non-overlapping-intervals - submission-11

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        import bisect
+        intervals.sort(key=lambda x:x[0])
+        from functools import cache
+        @cache
+        def dp(i):
+            if i >= len(intervals):
+                return 0
+            s, e = intervals[i]
+            j = bisect.bisect_left(intervals, [e, 0], lo = i + 1)
+            rm = j - i - 1
+            keep = rm + dp(j)
+            drop = 1 + dp(i + 1)
+            return min(keep, drop)
+        
+        return dp(0)
