6b2aeb16599dab781b73afc3d3bf79d5d9a42a12
Tran Huu Tin <thuutin@gmail.com>
2026-08-16 03:35:18 +0700

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        pq = [(0, k)]
+        D = {k: 0}
+        adj = defaultdict(list)
+        for u, v, w in times:
+            adj[u].append((v, w))
+        while pq:
+            time, node = heapq.heappop(pq)#.popleft()
+            for nei, weight in adj[node]:
+                if nei in D and D[nei] <= time + weight:
+                    continue
+                D[nei] = time + weight
+                heapq.heappush(pq, (D[nei], nei))
+        if len(D) == n:
+            return max(D.values())
+        return -1 
