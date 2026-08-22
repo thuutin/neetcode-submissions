10a4691a36997d3ddb8c24aa309913d7753a8060
Tran Huu Tin <thuutin@gmail.com>
2026-08-22 22:01:31 +0700

    Add: min-cost-to-connect-points - submission-3

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        nextPoint = points[0]
+        visited = set()
+        cost = 0
+        D = defaultdict(lambda : float("inf"))
+        D[tuple(nextPoint)] = 0
+        while nextPoint:
+            (x, y) = nextPoint
+            if (x, y) in visited:
+                continue
+            visited.add((x, y))
+            cost += D[(x, y)]
+            nextPoint = None
+            for x1, y1 in points:
+                d = abs(x1 - x) + abs(y1 - y)
+                D[(x1, y1)] = min(d, D[(x1, y1)])
+                if (x1, y1) not in visited:
+                    if nextPoint == None or D[nextPoint] > D[(x1, y1)]:
+                        nextPoint = (x1, y1)
+        return cost
