5a0b13e8e2439aabeac164835b8d7815ba8ecc92
Tran Huu Tin <thuutin@gmail.com>
2026-08-22 23:07:42 +0700

    Add: cheapest-flight-path - submission-0

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # k >= 0
+        # src, dst: there are some valid answers?
+        # node is (airport, # of stops)
+        # distance is the price
+        # edge is a flight
+        adj = defaultdict(list)
+        for u, v, p in flights:
+            adj[u].append((v, p))
+        pq = [(0, 1, src)]
+        prices = defaultdict(lambda : 1001 * len(flights))
+        prices[(1, src)] = 0
+        while pq:
+            price, stops, airport = heapq.heappop(pq)
+            node = stops, airport
+            if prices[node] < price:
+                continue
+            if airport == dst:
+                return price
+            for neibour, flight_price in adj[airport]:
+                if prices[(stops + 1, neibour)] < price + flight_price:
+                    continue
+                if stops + 1 > k + 2:
+                    continue
+                prices[(stops + 1, neibour)] = price + flight_price
+                heapq.heappush(pq, (price + flight_price, stops + 1, neibour))
+        return -1
+
