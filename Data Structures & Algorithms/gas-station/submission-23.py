aea3311a5df95aea26e581e861ff48fad5d0a9bc
Tran Huu Tin <thuutin@gmail.com>
2026-08-23 20:46:27 +0700

    Add: gas-station - submission-23

---
+++ Data Structures & Algorithms/gas-station/submission-23.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        #[-1, 0, -1, 3] [-1, 0, -1, 3]
+        #[-1, -1, -2, 1] [0, 0, -1, 3]
+        #     [0, -1, 2, 1] 1, 0, 3
+        #        [-1, 2, 1, 1] 0, 3
+        #             [3, 2, 2, 1] 4
+        
+        
+        n = len(gas)
+        current = 0
+        #print(arr)
+        i, j = 0, 0
+        while i < n:
+            if current + gas[j % n] - cost[j%n] >= 0:
+                current += gas[j % n] - cost[j%n]
+                if j - i + 1 >= n:
+                    return i
+                j += 1
+            elif i < j:
+                current -= (gas[i % n] - cost[i%n])
+                i += 1
+            else:
+                i += 1
+            j = max(j, i)
+        return -1
