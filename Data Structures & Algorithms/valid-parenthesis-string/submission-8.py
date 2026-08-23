76995db951e5ed80fd3247190c87de40258e80f7
Tran Huu Tin <thuutin@gmail.com>
2026-08-23 12:58:06 +0700

    Add: valid-parenthesis-string - submission-8

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        opened = 0
+        flexOpen, flexClose = 0, 0
+        for c in s:
+            if c == '(':
+                flexClose = min(opened, flexClose)
+                opened += 1
+            elif c == ')':
+                if opened > 0:
+                    opened -= 1
+                elif flexOpen > 0:
+                    flexClose -= 1
+                    flexOpen -= 1
+                else:
+                    return False
+            else:
+                flexOpen += 1
+                flexClose += 1
+            
+        return flexClose >= opened
