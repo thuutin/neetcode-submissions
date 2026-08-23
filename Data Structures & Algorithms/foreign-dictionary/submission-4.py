0aa8cc0a6aecf816e319b27e78e1d2077c2750e4
Tran Huu Tin <thuutin@gmail.com>
2026-08-23 10:30:16 +0700

    Add: foreign-dictionary - submission-4

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        indegree = defaultdict(int)
+        edges = defaultdict(set)
+        chars = set(words[0])
+        for i in range(1, len(words)):
+            prev, word = words[i-1], words[i]
+            if prev == word:
+                continue
+            if prev.startswith(word):
+                return ""
+            for j in range(min(len(word), len(prev))):
+                if word[j] == prev[j]:
+                    continue
+                if word[j] not in edges[prev[j]]:
+                    indegree[word[j]] += 1
+                    edges[prev[j]].add(word[j])
+                break
+            chars.update(word)
+        queue = deque([])
+        for c in chars:
+            if indegree[c] == 0:
+                queue.append(c)
+        res = []
+        while queue:
+            node = queue.popleft()
+            res.append(node)
+            for dependent in edges[node]:
+                indegree[dependent] -= 1
+                if indegree[dependent] <= 0:
+                    queue.append(dependent)
+        if len(res) != len(chars):
+            return ""
+        return "".join(res)
