33f0821c3e826e8414dd59a0db3f8b41fbe3aaa1
Tran Huu Tin <thuutin@gmail.com>
2026-08-25 13:32:55 +0700

    Add: stone-game-iii - submission-2

---
+++ Data Structures & Algorithms/stone-game-iii/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        from functools import cache
+        @cache
+        def dp(i, alice):
+            if i >= len(stoneValue):
+                return 0
+            neg = 1 if alice else -1
+            choices = []
+            s = 0
+            for j in range(i + 1, i + 4):
+                if j- 1 >= len(stoneValue):
+                    break 
+                s += stoneValue[j - 1]
+                choices.append(neg * s + dp(j, not alice))
+            if alice:
+                return max(choices)
+            else:
+                return min(choices)
+
+        #dp(i, alice_turn) returns the score of alice - score of bob
+        score = dp(0, True)
+        if score > 0:
+            return "Alice"
+        elif score == 0:
+            return "Tie"
+        else:
+            return "Bob"
