c5b8633bdcc7471ee0acd1af59d860f1f7badda6
Tran Huu Tin <thuutin@gmail.com>
2026-08-28 14:03:45 +0700

    Add: build-a-matrix-with-conditions - submission-11

---
+++ Data Structures & Algorithms/build-a-matrix-with-conditions/submission-11.py
@@ -0,0 +1,53 @@
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        # do 2 topological sortings of k vertexes with rol/col conditions as edges
+        # 
+        def toposort(edges):
+            indegree = defaultdict(int)
+            a = set(range(1, k + 1))
+            adj = defaultdict(list)
+            for u, v in edges:
+                indegree[v] += 1
+                if v in a:
+                    a.remove(v)
+                adj[u].append(v)
+            if not a:
+                return None
+            res = []
+            queue = deque(a)
+    
+            #print(queue)
+            while queue:
+                x = queue.popleft()
+                res.append(x)
+                while adj[x]:
+                    v = adj[x].pop()
+                    indegree[v] -= 1
+                    if indegree[v] <= 0:
+                        queue.append(v)
+            return res
+
+            # return a valid order or None if cycle exists
+        
+        # build the matrix
+        horizontal_order = toposort(colConditions)
+        if not horizontal_order:
+            return []
+        vertical_order = toposort(rowConditions)
+
+        if not vertical_order:
+            return []
+
+        cols = {}
+        rows = {}
+        for i, x in enumerate(vertical_order):
+            rows[x] = i
+        for i, x in enumerate(horizontal_order):
+            cols[x] = i
+        
+        res = [[0] * k for _ in range(k)]
+        for x in range(1, k + 1):
+            r = rows[x]
+            c = cols[x]
+            res[r][c] = x
+        return res
