765f7f4e0aa772e3407d7504fef286515322be3e
Tran Huu Tin <thuutin@gmail.com>
2026-09-02 20:46:13 +0700

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def sort(i, j):
+            if i >= j:
+                return
+            mid = (i + j) // 2
+            sort(i, mid)
+            sort(mid + 1, j)
+            a = i
+            b = mid + 1
+            arr = []
+            while a <= mid or b <= j:
+                if b > j or (a <= mid and nums[a] < nums[b]):
+                    arr.append(nums[a])
+                    a += 1
+                else:
+                    arr.append(nums[b])
+                    b += 1
+            #print(nums[i:j + 1], arr)
+            nums[i:j + 1] = arr
+
+        sort(0, len(nums) - 1)
+        return nums
