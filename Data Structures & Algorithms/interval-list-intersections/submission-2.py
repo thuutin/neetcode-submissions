0f36ac5c7cd62aa37953abd950332d3b08bd2cf3
Tran Huu Tin <thuutin@gmail.com>
2026-09-05 03:06:45 +0700

    Add: interval-list-intersections - submission-2

---
+++ Data Structures & Algorithms/interval-list-intersections/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        intersections = []
+        def intersection(first, second):
+
+            if first[0] > second[0]:
+                first, second = second, first
+            if second[0] <= first[1]:
+                return (second[0], min(first[1], second[1]) )
+            return None
+        for first in firstList:
+            for second in secondList:
+                inter = intersection(first, second)
+                if inter:
+                    intersections.append(inter)                
+        return intersections
