2eb8e0b65988a5be2f1e6305eaaf2e594786e873
Tran Huu Tin <thuutin@gmail.com>
2026-09-15 17:35:15 +0700

    Add: burst-balloons - submission-3

---
+++ Data Structures & Algorithms/burst-balloons/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        from functools import cache
+        @cache
+        def dp(i, j):
+            if i >= j:
+                if i > j:
+                    return 0
+                return nums[i] * nums[i - 1] * nums[i + 1]
+            max_coin = None
+            for last in range(i, j + 1):
+                burst = dp(i, last - 1) + dp (last + 1, j) + nums[last] * nums[i - 1] * nums[j + 1]
+                if max_coin == None or max_coin < burst:
+                    max_coin = burst
+            return max_coin
+
+        return dp(1, len(nums) - 2)
